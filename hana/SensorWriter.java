dc9b5cc992c87bd22b4a037858404713a58af4c3
rene <rene.kuebler@hs-aalen.de>
2016-04-09 13:06:51 +0200

    One Thread per Sensor

---
+++ hana/SensorWriter.java
@@ -0,0 +1,60 @@
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+/**
+ * Created by rene on 09.04.2016.
+ */
+
+class SensorWriter extends Thread {
+
+    private final Random rand = new Random();
+    private final int sid;
+    private final int n_values;
+    private Connection conn;
+    private PreparedStatement pstmt;
+
+    public SensorWriter(String connstr, String username, String password, int sensorid, int n_values) {
+        this.sid = sensorid;
+        this.n_values = n_values;
+        try {
+            this.conn = DriverManager.getConnection(connstr, username, password);
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            System.err.println("Connection Failed. Error: " + e);
+            return;
+        }
+        try {
+            this.pstmt = this.conn.prepareStatement("INSERT INTO test VALUES(?,?,?)");
+            this.pstmt.setInt(1, this.sid);
+        } catch (SQLException e) {
+            System.err.println("Prepare Statement Failed. Error: " + e);
+        }
+    }
+
+    private void insert() {
+        LocalDateTime ldt = LocalDateTime.now();
+        Timestamp sd = Timestamp.valueOf(ldt);
+        double value = rand.nextDouble();
+        try {
+            this.pstmt.setTimestamp(2, sd);
+            this.pstmt.setDouble(3, value);
+            this.pstmt.executeUpdate();
+            this.conn.commit(); // evt. hier nicht für jeden Insert commit ausführen
+        } catch (SQLException e) {
+            System.err.println("Execute Update Failed at Sensor " + this.sid + ". Error: " + e);
+        }
+    }
+
+    public void run() {
+        for (int i = 0; i < this.n_values; i++) {
+            this.insert();
+        }
+        try {
+            this.conn.commit();
+            this.conn.close();
+        } catch (SQLException e) {
+            System.err.println("Last commit failed at Sensor " + this.sid + ". Error: " + e);
+        }
+    }
+}
--- hana/perftest.java
+++ hana/perftest.java
@@ -3,19 +3,9 @@
  * for IoT Sensordata
  */
 
-import java.sql.Timestamp;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
-import java.time.LocalDateTime;
-import java.util.Random;
 
 public class perftest {
     public static void main(String[] argv) {
-
-        Connection connection;
 
         final String myname = "KK_STUDENT_030";
         final String mysecret = "M7YaGrq4D3W6aG";
@@ -25,102 +15,43 @@ public class perftest {
         final String connstr = "jdbc:sap://" + host + ":" + port + options;
         System.out.println("Connection String: " + connstr);
 
-
         final int iValues = 2; //100000;
-        final int iSensors = 20;
+        final int iSensors = 2;
         final int VALUE_TYPE_TEMP_CENTIGRADE = 42;
         String tableName = "test";
 
-        double value;
-        int sensor_id;
+        final long startTime = System.currentTimeMillis();
+        SensorWriter[] s_threads = new SensorWriter[iSensors];
 
-        try {
-            connection = DriverManager.getConnection(connstr, myname, mysecret);
-        } catch (SQLException e) {
-            System.err.println("Connection Failed. User/Passwd Error? Error: " + e);
-            return;
+        // Start a thread for every Sensor
+        for (int i = 0; i < iSensors; i++) {
+            s_threads[i] = new SensorWriter(connstr, myname, mysecret, i, iValues);
+            s_threads[i].start();
         }
-        if (connection != null) {
-            try {
-                System.out.println("Connection to HANA successful!");
-                Statement stmt = connection.createStatement();
-//                ResultSet resultSet = stmt.executeQuery("Select 'hello world' from dummy");
-//                resultSet.next();
-//                String hello = resultSet.getString(1);
-//                System.out.println(hello);
 
-                final long startTime = System.currentTimeMillis();
-                for (int i = 0; i < iValues; i++) {
-                    //LocalDateTime dt = LocalDateTime.now();
-                    for (int j = 0; j < iSensors; j++) {
-                        sensor_id = j;
-                        value = i + j;
-                        LocalDateTime dt = LocalDateTime.now();
-                        /*System.out.print(sensor_id);
-                        System.out.print(' ');
-                        System.out.print(dt);
-                        System.out.print(' ');
-                        System.out.println(value);*/
+        // Wait till all Threads are finished
+        outerloop:
+        while (true) {
+            for (int i = 0; i < iSensors; i++) {
+                if (s_threads[i].isAlive()) {
+                    try {
+                        Thread.sleep(100);
+                        continue outerloop;
+                    } catch (InterruptedException e) {
+                        System.err.println("Error while waiting for Threads to finish. Error: " + e);
                     }
+                } else {
+                    break outerloop;
                 }
-                final int lines = iValues * iSensors;
-                final long endTime = System.currentTimeMillis();
-                final double delta = (double) endTime - (double) startTime;
-
-                System.out.printf("Runtime: %f.\n", delta);
-                System.out.printf("Time per Insert: %8.8f\n", delta / lines);
-                System.out.printf("Inserts per Second: %8.8f\n", 1 / (delta / lines) * 1000);
-            } catch (SQLException e) {
-                System.err.println("Query failed!");
             }
         }
+
+        final int lines = iValues * iSensors;
+        final long endTime = System.currentTimeMillis();
+        final double delta = (double) endTime - (double) startTime;
+
+        System.out.printf("Runtime: %f.\n", delta);
+        System.out.printf("Time per Insert: %8.8f\n", delta / lines);
+        System.out.printf("Inserts per Second: %8.8f\n", 1 / (delta / lines) * 1000);
     }
-
-    private class SensorWriter {
-
-        Random rand = new Random();
-        private Connection conn;
-        private int sid;
-        private int n_values;
-        private PreparedStatement pstmt;
-
-        public SensorWriter(String connstr, String username, String password, int sensorid, int n_values) {
-            this.sid = sensorid;
-            this.n_values = n_values;
-            try {
-                this.conn = DriverManager.getConnection(connstr, username, password);
-            } catch (SQLException e) {
-                System.err.println("Connection Failed. Error: " + e);
-                return;
-            }
-            try {
-                this.pstmt = this.conn.prepareStatement("INSERT INTO test VALUES(?,?,?)");
-                this.pstmt.setInt(1, this.sid);
-            } catch (SQLException e) {
-                System.err.println("Prepare Statement Failed. Error: " + e);
-            }
-        }
-
-        private void insert() {
-            LocalDateTime ldt = LocalDateTime.now();
-            Timestamp sd = Timestamp.valueOf(ldt);
-            double value = rand.nextDouble();
-            try {
-                this.pstmt.setTimestamp(2, sd);
-                this.pstmt.setDouble(3, value);
-                this.pstmt.executeUpdate();
-            } catch (SQLException e) {
-                System.err.println("Execute Update Failed at Sensor " + this.sid + ". Error: " + e);
-            }
-        }
-
-        public void run() {
-            for (int i = 0; i < this.n_values; i++) {
-                this.insert();
-            }
-        }
-    }
-
-
-
 }
