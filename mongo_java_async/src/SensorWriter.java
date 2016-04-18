dfa3e4076e510ea5e7620c0a76fb0b0b9fa8276b
René Kübler <rnkblr@gmail.com>
2016-04-18 18:39:00 +0200

    mongodb async version 3.2

---
+++ mongo_java_async/src/SensorWriter.java
@@ -0,0 +1,57 @@
+import com.mongodb.async.client.MongoClient;
+import com.mongodb.async.client.MongoClients;
+import com.mongodb.async.client.MongoCollection;
+import com.mongodb.async.client.MongoDatabase;
+import org.bson.Document;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
+
+//import com.mongodb.async.SingleResultCallback;
+
+class SensorWriter extends Thread {
+
+    private final Random rand = new Random();
+    private final int sid;
+    private final int n_values;
+    private final int buffSize;
+    private final MongoClient mClient;
+    private final MongoCollection coll;
+    private final ArrayList<Document> batch;
+
+
+    public SensorWriter(String connstr, int sensorid, int n_values, int buffSize) {
+        this.sid = sensorid;
+        this.n_values = n_values;
+        this.buffSize = buffSize;
+        batch = new ArrayList<>(buffSize);
+        mClient = MongoClients.create(connstr);
+        MongoDatabase db = mClient.getDatabase("mydb");
+        coll = db.getCollection(String.valueOf(sid));
+    }
+
+    private Document insert() {
+        LocalDateTime ldt = LocalDateTime.now();
+        Timestamp sd = Timestamp.valueOf(ldt);
+        double value = rand.nextDouble();
+
+        return new Document("sid", sid)
+                .append("dt", sd)
+                .append("value", value);
+    }
+
+    public void run() {
+
+        for (int i = 0; i < this.n_values; i++) {
+            batch.add(insert());
+            if (i % buffSize == 0) {
+                coll.insertMany(batch, (aVoid, throwable) -> {
+                }); // no Callback necessary
+                batch.clear();
+            }
+        }
+        mClient.close();
+    }
+}
---
+++ mongo_java_async/src/perftest.java
@@ -0,0 +1,63 @@
+/**
+ * mysql write performance
+ * for IoT Sensordata
+ */
+
+
+public class perftest {
+    public static void main(String[] argv) {
+        final String connstr = "mongodb://localhost";
+        System.out.println("Connection String: " + connstr);
+
+        final int iValues;
+        final int iSensors;
+        final int buffSize;
+        if (argv.length < 3) {
+            iValues = 100000;
+            iSensors = 10;
+            buffSize = 1000;
+        } else {
+            iValues = Integer.parseInt(argv[0]); // Values pro Sensor
+            iSensors = Integer.parseInt(argv[1]);
+            buffSize = Integer.parseInt(argv[2]);
+        }
+
+        final long startTime = System.currentTimeMillis();
+        SensorWriter[] s_threads = new SensorWriter[iSensors];
+
+        // Start a thread for every Sensor
+        for (int i = 0; i < iSensors; i++) {
+            s_threads[i] = new SensorWriter(connstr, i, iValues, buffSize);
+            s_threads[i].start();
+        }
+
+        // Wait till all Threads are finished
+        outerloop:
+        while (true) {
+            int alive_count = iSensors;
+            for (int i = 0; i < iSensors; i++) {
+                if (s_threads[i].isAlive()) {
+                    try {
+                        Thread.sleep(100);
+                        continue outerloop;
+                    } catch (InterruptedException e) {
+                        System.err.println("Error while waiting for Threads to finish. Error: " + e);
+                    }
+                } else if (!(s_threads[i].isAlive()) && alive_count <= 1) {
+                    break outerloop;
+                } else {
+                    alive_count--;
+                }
+            }
+        }
+
+
+        final int lines = iValues * iSensors;
+        final long endTime = System.currentTimeMillis();
+        final double delta = (double) endTime - (double) startTime;
+
+        System.out.printf("Runtime: %f.\n", delta);
+        System.out.printf("Time per Insert: %8.8f\n", delta / lines);
+        System.out.printf("Inserts per Second: %8.8f\n", 1 / (delta / lines) * 1000);
+    }
+}
